cd16064bb18c4d8fdf90b45428760159cc6b0cbc
alexey.bukin <alexeybookin@gmail.com>
2014-12-23 18:43:48 +0300

    audio change

--- app/src/main/java/com/help/media/mediah3lp/Audio.java
+++ app/src/main/java/com/help/media/mediah3lp/Audio.java
@@ -1,5 +1,7 @@
 package com.help.media.mediah3lp;
 
+import java.util.Comparator;
+
 /**
  * Created by alexey.bukin on 17.12.2014.
  */
@@ -20,4 +22,32 @@ public class Audio {
 //        public int getGenre() {
 //            return genre;
 //        }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Audio)) return false;
+
+        Audio audio = (Audio) o;
+
+        if (artist != null ? !artist.equals(audio.artist) : audio.artist != null) return false;
+
+        return true;
     }
+
+    @Override
+    public int hashCode() {
+        return artist != null ? artist.hashCode() : 0;
+    }
+
+    public static class AudioComparator implements Comparator<Audio>{
+
+        @Override
+        public int  compare(Audio lhs, Audio rhs) {
+            String stringL = lhs.getArtist() == null ? "" : lhs.getArtist().trim();
+            String stringR = rhs.getArtist() == null ? "" : rhs.getArtist().trim();
+            return stringL.compareTo(stringR);
+        }
+    }
+}
